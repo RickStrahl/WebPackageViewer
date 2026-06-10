d1955533a658fbf76d813f0b46a9bc385b9005bf
Rick Strahl <rstrahl@west-wind.com>
2026-06-09 17:14:29 -0700

    Add Window styling to the main window.

--- WebPackageViewer/MainWindow.xaml.cs
+++ WebPackageViewer/MainWindow.xaml.cs
@@ -1,11 +1,11 @@
 using Microsoft.Web.WebView2.Core;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Security.Policy;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace WebPackageViewer
 {
@@ -23,6 +23,15 @@ namespace WebPackageViewer
             Configuration = config;
 
             InitializeComponent();
+
+            Loaded += Window_Loaded;
+            StateChanged += Window_StateChanged;
+            Activated += Window_Activated;
+            Deactivated += Window_Deactivated;
+            TitleBar.MouseRightButtonUp += TitleBar_MouseRightButtonUp;
+            SystemMenuButton.Click += SystemMenuButton_Click;
+            SystemMenuButton.MouseDoubleClick += SystemMenuButton_MouseDoubleClick;
+
             InitWebView();
 
             ExeFile = System.Reflection.Assembly.GetExecutingAssembly().Location;
@@ -38,9 +47,11 @@ namespace WebPackageViewer
                     Width = width;
                     Height = height;
                 }
-            }            
+            }
 
-        }        
+            Closed += MainWindow_Closed;
+            SystemEvents.UserPreferenceChanged += SystemEvents_UserPreferenceChanged;
+        }
 
 
         private async void InitWebView()
@@ -199,5 +210,133 @@ namespace WebPackageViewer
                 {".zip", "application/zip"}
           #endregion
       };
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ApplyThemeFromSystem();
+            UpdateMaximizeRestoreGlyph();
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            UpdateMaximizeRestoreGlyph();
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            UpdateActivationVisualState(true);
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            UpdateActivationVisualState(false);
+        }
+
+        private void SystemMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            var point = PointToScreen(new Point(0, TitleBar.ActualHeight));
+            SystemCommands.ShowSystemMenu(this, point);
+        }
+
+        private void SystemMenuButton_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void TitleBar_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var screenPoint = PointToScreen(e.GetPosition(this));
+            SystemCommands.ShowSystemMenu(this, screenPoint);
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                SystemCommands.RestoreWindow(this);
+            else
+                SystemCommands.MaximizeWindow(this);
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        private void UpdateMaximizeRestoreGlyph()
+        {
+            if (MaximizeRestoreButton == null)
+                return;
+
+            MaximizeRestoreButton.Content = WindowState == WindowState.Maximized ? "\uE923" : "\uE922";
+        }
+
+        private void SystemEvents_UserPreferenceChanged(object sender, UserPreferenceChangedEventArgs e)
+        {
+            if (e.Category == UserPreferenceCategory.Color ||
+                e.Category == UserPreferenceCategory.General ||
+                e.Category == UserPreferenceCategory.VisualStyle)
+            {
+                Dispatcher.Invoke(ApplyThemeFromSystem);
+            }
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            SystemEvents.UserPreferenceChanged -= SystemEvents_UserPreferenceChanged;
+            Activated -= Window_Activated;
+            Deactivated -= Window_Deactivated;
+            Closed -= MainWindow_Closed;
+        }
+
+        private void ApplyThemeFromSystem()
+        {
+            var isLightTheme = IsLightThemeEnabled();
+
+            SetBrushColor("WindowBackgroundBrush", isLightTheme ? Color.FromRgb(248, 248, 248) : Color.FromRgb(45, 45, 48));
+            SetBrushColor("WindowBorderBrush", Colors.Transparent);
+            SetBrushColor("ActiveTitleBarBackgroundBrush", isLightTheme ? Color.FromRgb(245, 245, 245) : Color.FromRgb(30, 30, 30));
+            SetBrushColor("TitleBarInactiveBackgroundBrush", isLightTheme ? Color.FromRgb(241, 241, 241) : Color.FromRgb(36, 36, 38));
+            SetBrushColor("TitleBarForegroundBrush", isLightTheme ? Color.FromRgb(32, 32, 32) : Color.FromRgb(245, 245, 245));
+            SetBrushColor("TitleBarButtonBackgroundBrush", Colors.Transparent);
+            SetBrushColor("TitleBarButtonHoverBrush", isLightTheme ? Color.FromArgb(18, 0, 0, 0) : Color.FromArgb(36, 255, 255, 255));
+            SetBrushColor("TitleBarButtonPressedBrush", isLightTheme ? Color.FromArgb(34, 0, 0, 0) : Color.FromArgb(54, 255, 255, 255));
+            SetBrushColor("TitleBarButtonForegroundBrush", isLightTheme ? Color.FromRgb(32, 32, 32) : Color.FromRgb(238, 238, 238));
+
+            UpdateActivationVisualState(IsActive);
+        }
+
+        private void SetBrushColor(string resourceKey, Color color)
+        {
+            Resources[resourceKey] = new SolidColorBrush(color);
+        }
+
+        private void UpdateActivationVisualState(bool isActive)
+        {
+            if (TitleBar == null)
+                return;
+
+            var key = isActive ? "ActiveTitleBarBackgroundBrush" : "TitleBarInactiveBackgroundBrush";
+            if (Resources[key] is SolidColorBrush brush)
+                TitleBar.Background = brush;
+        }
+
+        private static bool IsLightThemeEnabled()
+        {            
+            const string personalizeKey = @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+
+            using (var key = Registry.CurrentUser.OpenSubKey(personalizeKey))
+            {
+                var value = key?.GetValue("AppsUseLightTheme");
+                if (value is int appTheme)
+                    return appTheme > 0;
+            }
+
+            return true;
+        }
     }
 }
